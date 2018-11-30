c9946d2955a7c2f3cbbf3957ef82120ef95bc69b
boweny <bowen.yang@sambanovasystems.com>
2018-11-29 18:10:36 -0800

    clean up code

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.backends.cudnn as cudnn
 from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import DataLoader
 
